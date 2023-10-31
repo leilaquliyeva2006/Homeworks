56ac27c1a05d53ebd1119e90bbf5e2c56baad895
leila <leylag659@gmail.com>
2023-10-31 16:44:53 +0400

    math_func

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-function func(n) {
-  if (n <= 0) {
-    return 0;
-  } else if (n === 1) {
-    return 1;
-  } else {
-    return func(n - 1) + func(n - 2);
-  }
+// 1. Проверку на то что переданная операция - это функция. Напрмер Math есть константы типа Math.PI и если я щас передам 'PI'
+//    как `oper`, то он его найдёт и выведет ошибку, а надо чтобы просто вернул значение Math.PI
+
+// 2. Если передана не существушая операция (ни в Math ни в switch), то вывести ошибку, что такой операции нет
+
+// 3. Написать логику для multiply, division и subtract. Начальные значения 1, 1 и 0 соответсвенно
+
+
+function Math_Func(oper, num1, num2) {
+    if (typeof oper === 'function') {
+        return oper(num1, num2);
+    } else if (Math.hasOwnProperty(oper)) {
+        return Math[oper];
+    } else {
+        switch (oper) {
+            case 'multiply':
+                return num1 * num2;
+            case 'division':
+                if (num2 !== 0) {
+                    return num1 / num2;
+                } else {
+                    return 'Error! Tried to divide by 0!';
+                }
+            case 'subtract':
+                return num1 - num2;
+            default:
+                return 'Error! Dont have this operation!';
+        }
+    }
 }
 
-console.log(func(0));
-console.log(func(1));
-console.log(func(16));
+console.log(Math_Func('PI', 0, 0));
+console.log(Math_Func('multiply', 5, 10));
+console.log(Math_Func('division', 8, 0)); 
+console.log(Math_Func('default', 0, 0));
