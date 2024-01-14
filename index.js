46c1e880be78c8ba3c1264fccac5c25f78502c86
leila <leylag659@gmail.com>
2024-01-14 20:09:21 +0400

    homework_16

--- index.js
+++ index.js
@@ -1,46 +1,16 @@
-document.getElementById("addRowBtn").addEventListener("click", addRow);
-document.getElementById("addColumnBtn").addEventListener("click", addColumn);
-document.getElementById("deleteRowBtn").addEventListener("click", deleteRow);
-document
-  .getElementById("deleteColumnBtn")
-  .addEventListener("click", deleteColumn);
+function createTable(n, m) {
+  var table = document.createElement("table");
 
-function addRow() {
-  let table = document.getElementById("myTable");
-  let newRow = table.insertRow();
+  for (var i = 0; i < n; i++) {
+    var row = table.insertRow();
 
-  let cell1 = newRow.insertCell(0);
-  let cell2 = newRow.insertCell(1);
-
-  cell1.textContent = "Новая строка, Ячейка 1";
-  cell2.textContent = "Новая строка, Ячейка 2";
-}
-
-function addColumn() {
-  let table = document.getElementById("myTable");
-
-  for (let i = 0; i < table.rows.length; i++) {
-    let cell = table.rows[i].insertCell(-1);
-    cell.textContent = "Новая строка, Ячейка " + table.rows[i].cells.length;
-  }
-}
-
-function deleteRow() {
-  let table = document.getElementById("myTable");
-  let lastRow = table.rows.length - 1;
-
-  if (lastRow > 0) {
-    table.deleteRow(lastRow);
-  }
-}
-
-function deleteColumn() {
-  let table = document.getElementById("myTable");
-  let lastColumn = table.rows[0].cells.length - 1;
-
-  if (lastColumn > 0) {
-    for (let i = 0; i < table.rows.length; i++) {
-      table.rows[i].deleteCell(lastColumn);
+    for (var j = 0; j < m; j++) {
+      var cell = row.insertCell();
+      cell.textContent = "Строка " + (i + 1) + ", Ячейка " + (j + 1);
     }
   }
+
+  document.body.appendChild(table);
 }
+
+createTable(3, 4);
